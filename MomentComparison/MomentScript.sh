f7b91240600806ac974ec8f0b7c0638938595e90
jguerreromontero <105151602+jguerreromontero@users.noreply.github.com>
2022-08-24 16:28:03 +0100

    Create MomentScript.sh

---
+++ MomentComparison/MomentScript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Script for the Comparison of both BwS approximations
+#
+#SBATCH --job-name=BwSAccuracyComparison
+#SBATCH --partition=short
+#SBATCH --time=12:00:00
+#SBATCH --array=1-6
+#
+########################################################################
+ 
+# Decide which input file this Slurm task will process.
+# We use the special $SLURM_ARRAY_TASK_ID variable, which tells the
+# task which one it is among the whole job array.
+input_file=Parameters${SLURM_ARRAY_TASK_ID}
+output_file=output${SLURM_ARRAY_TASK_ID}
+filename="Parameters${SLURM_ARRAY_TASK_ID}"
+
+./ArtificialComparisonMoments.exe $filename
