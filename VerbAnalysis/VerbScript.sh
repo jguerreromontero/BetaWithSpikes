68d75af8a71645af05dab4b818863ec641993a3d
jguerreromontero <105151602+jguerreromontero@users.noreply.github.com>
2023-02-13 20:34:15 +0000

    Update VerbScript.sh

--- VerbAnalysis/VerbScript.sh
+++ VerbAnalysis/VerbScript.sh
@@ -12,8 +12,8 @@
 # Decide which input file this Slurm task will process.
 # We use the special $SLURM_ARRAY_TASK_ID variable, which tells the
 # task which one it is among the whole job array.
-input_file=DATA${SLURM_ARRAY_TASK_ID}
-output_file=output${SLURM_ARRAY_TASK_ID}
-filename="DATA${SLURM_ARRAY_TASK_ID}"
+input_file=DATA${SLURM_ARRAY_TASK_ID}.txt
+output_file=output${SLURM_ARRAY_TASK_ID}.txt
+filename="DATA${SLURM_ARRAY_TASK_ID}.txt"
 
 ./VerbAnalysis.exe $filename
