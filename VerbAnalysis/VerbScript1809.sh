9361a44289d8d8431f97e43cd0d2a014e79cce32
jguerreromontero <105151602+jguerreromontero@users.noreply.github.com>
2023-02-15 11:44:50 +0000

    Create VerbScript1809.sh

---
+++ VerbAnalysis/VerbScript1809.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Script for the Comparison of both BwS approximations
+#
+#SBATCH --job-name=VerbAnalysis
+#SBATCH --partition=short
+#SBATCH --time=12:00:00
+#SBATCH --array=1-4
+#
+########################################################################
+ 
+# Decide which input file this Slurm task will process.
+# We use the special $SLURM_ARRAY_TASK_ID variable, which tells the
+# task which one it is among the whole job array.
+input_file=DATA1809${SLURM_ARRAY_TASK_ID}.txt
+output_file=output${SLURM_ARRAY_TASK_ID}.txt
+filename="DATA1809${SLURM_ARRAY_TASK_ID}.txt"
+
+./VerbAnalysis.exe $filename
