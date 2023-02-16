51115d2ac60e96ce5ea2bb3135a6ed47f57fb28c
jguerreromontero <105151602+jguerreromontero@users.noreply.github.com>
2023-02-16 14:11:40 +0000

    Create LangScript.sh

---
+++ VerbAnalysis/LangScript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Script for the Comparison of both BwS approximations
+#
+#SBATCH --job-name=VerbAnalysis
+#SBATCH --partition=short
+#SBATCH --time=12:00:00
+#SBATCH --array=1-3
+#SBATCH --mem=10M
+#
+########################################################################
+ 
+# Decide which input file this Slurm task will process.
+# We use the special $SLURM_ARRAY_TASK_ID variable, which tells the
+# task which one it is among the whole job array.
+input_file=LangData${SLURM_ARRAY_TASK_ID}.txt
+output_file=output${SLURM_ARRAY_TASK_ID}.txt
+filename="LangData${SLURM_ARRAY_TASK_ID}.txt"
+
+./VerbAnalysis.exe $filename
