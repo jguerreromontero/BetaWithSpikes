9a7afc603e9baace08227bfe9f46012af9c1b644
jguerreromontero <105151602+jguerreromontero@users.noreply.github.com>
2023-02-17 13:09:56 +0000

    Update ReformSliceScript.sh

--- ReformDetection/ReformSliceScript.sh
+++ ReformDetection/ReformSliceScript.sh
@@ -13,8 +13,8 @@
 # Decide which input file this Slurm task will process.
 # We use the special $SLURM_ARRAY_TASK_ID variable, which tells the
 # task which one it is among the whole job array.
-input_file=ReformSliceDATA${SLURM_ARRAY_TASK_ID}.txt
+input_file=ReformDATASlice${SLURM_ARRAY_TASK_ID}.txt
 output_file=output${SLURM_ARRAY_TASK_ID}.txt
-filename="ReformSliceDATA${SLURM_ARRAY_TASK_ID}.txt"
+filename="ReformDATASlice${SLURM_ARRAY_TASK_ID}.txt"
 
 ./LRTimeDivision.exe $filename
